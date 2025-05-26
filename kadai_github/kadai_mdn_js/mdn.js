0832e73cc53e72c34ee5900fd8175be8cf82cb89
mijyumaru777-mow <misuzu.gyo_m7@icloud.com>
2025-05-26 15:25:05 +0900

    kadai_mdn_jsを変更後に追加

--- kadai_github/kadai_mdn_js/mdn.js
+++ kadai_github/kadai_mdn_js/mdn.js
@@ -3,13 +3,13 @@ const today = new Date();
 
 //日付の各部分を取得
 const [month, day, year,] = [
-    date.getMonth() + 1, //月は0から始まるため、+1
-    date.getDate(),
-    date.getFullYear(),    
+    today.getMonth() + 1, //月は0から始まるため、+1
+    today.getDate(),
+    today.getFullYear(),    
 ];
 
 //日付をコンソールに出力
-console.log('本日の日付は: ${year}年${month}月${day}日');
+console.log(`本日の日付は: ${year}年${month}月${day}日`);
 
 
  
